b058538a575187aca6358b882b2e556a0f64a61c
rahul <rahulmp.583@gmail.com>
2024-07-15 22:45:38 +0530

    BDD framework is created

---
+++ Desktop/Chakrapani/Revision/BDDFramwork1/src/main/java/BDDFramwork1/BDDFramwork1/App.java
@@ -0,0 +1,13 @@
+package BDDFramwork1.BDDFramwork1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
